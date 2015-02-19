6aa840c535e08f97f61d2677b1c67685898c38cf
Nepster <nepsterxxx@mail.ru>
2015-02-19 22:28:51 +0200

    update

---
+++ Installer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace nepster\modules\installer;
+
+use yii\helpers\Console;
+use yii\log\Logger;
+use Yii;
+
+/**
+ * Install yii2 base modules
+ */
+class Installer extends \yii\console\Controller
+{
+    /**
+     * Установка модуля
+     */
+    public function actionIndex()
+    {
+        echo 1;
+    }
+}
